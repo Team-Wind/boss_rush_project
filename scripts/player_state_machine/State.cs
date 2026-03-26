39fd93ff3e1edc96ed515acdc157dbe6990ca783
caio lobato <caiolpbarreto@gmail.com>
2026-03-26 17:39:52 -0300

    criação incial de arquivos abstratos e da maquina de estados, alem de estados do player (sem implementação)

---
+++ scripts/player_state_machine/State.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+//essa é a classe abstrata que usaremos para construir todos os estados do player, ela tem métodos para entrar, sair, atualizar, atualizar física e lidar com input
+
+public abstract partial class State : Node
+{
+	public StateMachine stateMachine;
+	public virtual void Enter()
+	{
+	}
+
+	public virtual void Exit()
+	{
+	}
+
+	public virtual void Update(double delta)
+	{
+	}
+
+	public virtual void PhysicsUpdate(double delta)
+	{
+	}
+
+	public virtual void HandleInput(InputEvent inputEvent)
+	{
+	}
+
+
+}
