832e08aa846bb818fb362e317c3970380e84cb2c
caio lobato <caiolpbarreto@gmail.com>
2026-04-04 22:09:03 -0300

    .

--- scripts/player/player_actions/AttackState.cs
+++ scripts/player/player_actions/AttackState.cs
@@ -6,10 +6,21 @@ public partial class AttackState : State
 	[Export] public Player Player;
 	public override void Enter()
 	{
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void PhysicsUpdate(double delta)
 	{
 	}
+
+	public override void HandleInput(InputEvent @event)
+	{
+		
+	}
+
+    public override void Exit()
+    {
+    }
+
 }
